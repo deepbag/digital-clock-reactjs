6c52a48989317a9e7d1bbca531cc7444ec9d2e69
deepbag <deepbag92@gmail.com>
2020-10-22 15:03:54 +0530

    digital clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    
+    {/* Date */}
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+
+    {/* Time */}
+    const time = new Date().toLocaleTimeString();
+
+    const [currtime, setcurrtime] = useState(time);
+
+    const updatetime = () => {
+        const time = new Date().toLocaleTimeString();
+        setcurrtime(time);
+    }
+
+    setInterval(updatetime, 1000);
+
+    return(
+        <>
+          <div className="conatiner text-center d-flex justify-content-center">
+            <div className="row my-5 py-5 ">
+                <div className="col-lg-4">
+                    <strong style={{fontSize: "70px"}} className="border-con p-4 text-white">{date}</strong>
+                </div>
+                <div className="col-lg-4">
+                    <strong style={{fontSize: "70px"}} className="border-con p-4 text-white">0{month}</strong>
+                </div>
+                <div className="col-lg-4">
+                    <strong style={{fontSize: "70px"}} className="border-con p-4 text-white">{year}</strong>
+                </div>
+            </div>
+          </div>
+          <div className="container d-flex justify-content-center">
+            <div className="row my-5 py-5">
+                <div className="col-lg-12">
+                    <strong style={{fontSize: "70px"}} className="border-con p-4 text-white">{currtime}</strong>
+                </div>
+            </div>
+          </div>
+        </>
+    )
 }
 
 export default App;
