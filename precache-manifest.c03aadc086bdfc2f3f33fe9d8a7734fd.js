72f63657d25fa9daea3f4ee8f42accf08de47fe5
deepbag <deepbag92@gmail.com>
2020-10-22 15:33:51 +0530

    Updates

---
+++ precache-manifest.c03aadc086bdfc2f3f33fe9d8a7734fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ad9224de1f25055a48052606cd11ac",
+    "url": "/digital-clock-reactjs/index.html"
+  },
+  {
+    "revision": "4c131233adb46c7824a0",
+    "url": "/digital-clock-reactjs/static/css/main.86577a74.chunk.css"
+  },
+  {
+    "revision": "2252fcfd4cacada37efd",
+    "url": "/digital-clock-reactjs/static/js/2.79c74042.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/digital-clock-reactjs/static/js/2.79c74042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c131233adb46c7824a0",
+    "url": "/digital-clock-reactjs/static/js/main.890bb08a.chunk.js"
+  },
+  {
+    "revision": "3cbfec6462eca54c353c",
+    "url": "/digital-clock-reactjs/static/js/runtime-main.05abe400.js"
+  }
+]);
